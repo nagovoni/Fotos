b1d6388040db18a2e75336052af58774ab82cffd
nagovoni <natalia.govoni@360suites.com.br>
2024-06-07 16:25:02 -0300

    carousel e gallery ok

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,23 @@
       <v-container>
         <v-row>
           <v-col cols="12">
-            <v-sheet min-height="70vh" rounded="lg">
+            
+            <v-sheet>
+              <v-card-title>{{ roomData.property.name }}</v-card-title>
               <v-carousel height="100%" show-arrows="hover" cycle hide-delimiter-background>
                 <v-carousel-item v-for="(image, i) in roomData.property.propertyImages" :key="i">
-                  <v-sheet height="700px">
-                    <img :src="image.url" :alt="'Slide' + (i + 1)" />
+                  <v-sheet height="500px">
+                    <img :src="image.url" :alt="'Slide' + (i + 1)" width="1200px" height="500px" />
                   </v-sheet>
                 </v-carousel-item>
               </v-carousel>
             </v-sheet>
           </v-col>
 
-          <v-col cols="12" md="6">
+          <v-col cols="12">
+            
             <v-card>
+              <v-card-title>{{ roomData.displayName }}</v-card-title>
               <v-card-text>
                 <v-container fluid>
                   <v-row>
@@ -26,7 +30,6 @@
                   </v-row>
                 </v-container>
               </v-card-text>
-              <v-card-title>{{ roomData.displayName }}</v-card-title>
               <v-card-text>
                 <p><strong>Address:</strong> {{ roomData.property.address }}</p>
                 <p><strong>City:</strong> {{ roomData.property.city }}</p>
@@ -45,5 +48,5 @@
 <script setup>
 import { ref } from 'vue'
 import roomData from '../package.json'
-const slides = ref(['First', 'Second', 'Third', 'Fourth', 'Fifth'])
+
 </script>
